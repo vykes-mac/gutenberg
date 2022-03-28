59b4b3899b6436dd76e9bc5c10f9d50a91067048
Riad Benguella <benguella@gmail.com>
2022-03-28 11:02:57 +0100

    List block: Add migration from v1 to v2

--- packages/block-library/src/list/v2/edit.js
+++ packages/block-library/src/list/v2/edit.js
@@ -5,6 +5,7 @@ import {
 	BlockControls,
 	useBlockProps,
 	useInnerBlocksProps,
+	store as blockEditorStore,
 } from '@wordpress/block-editor';
 import { ToolbarButton } from '@wordpress/components';
 import { isRTL, __ } from '@wordpress/i18n';
@@ -14,20 +15,53 @@ import {
 	formatListNumbered,
 	formatListNumberedRTL,
 } from '@wordpress/icons';
+import { useEffect } from '@wordpress/element';
+import { useRegistry, useDispatch } from '@wordpress/data';
 
 /**
  * Internal dependencies
  */
 import OrderedListSettings from '../ordered-list-settings';
+import { migrateToListV2 } from './migrate';
 
 const TEMPLATE = [ [ 'core/list-item' ] ];
 
-function Edit( { attributes, setAttributes } ) {
+/**
+ * At the moment, deprecations don't handle create blocks from attributes
+ * (like when using CPT templates). For this reason, this hook is necessary
+ * to avoid breaking templates using the old list block format.
+ *
+ * @param {Object} attributes Block attributes.
+ * @param {string} clientId   Block client ID.
+ */
+function useMigrateOnLoad( attributes, clientId ) {
+	const registry = useRegistry();
+	const { updateBlockAttributes, replaceInnerBlocks } = useDispatch(
+		blockEditorStore
+	);
+
+	useEffect( () => {
+		// As soon as the block is loaded, migrate it to the new version.
+
+		if ( ! attributes.values ) {
+			return;
+		}
+		const [ newAttributes, newInnerBlocks ] = migrateToListV2( attributes );
+
+		registry.batch( () => {
+			updateBlockAttributes( clientId, newAttributes );
+			replaceInnerBlocks( clientId, newInnerBlocks );
+		} );
+	}, [ attributes.values ] );
+}
+
+function Edit( { attributes, clientId, setAttributes } ) {
 	const blockProps = useBlockProps();
 	const innerBlocksProps = useInnerBlocksProps( blockProps, {
 		allowedBlocks: [ 'core/list-item' ],
 		template: TEMPLATE,
 	} );
+	useMigrateOnLoad( attributes, clientId );
 	const { ordered, reversed, start } = attributes;
 	const TagName = ordered ? 'ol' : 'ul';
 
